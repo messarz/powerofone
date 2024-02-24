d95fc7b467f6c6922077f9bfc84e11267bdbb193
Ariez <arzmess@gmail.com>
2024-02-24 20:07:26 +0800

    init
    commit

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,7 +1,8 @@
 
 import { component$ } from "@builder.io/qwik";
 
-export const Footer = component$(() => ( <div class="mx-auto max-w-7xl px-6 py-12 md:flex md:items-center md:justify-between lg:px-8"> <div class="flex justify-center space-x-6 md:order-2"> <div class="mt-8 md:order-1 md:mt-0"> <p class="truncate text-sm text-gray-300">2024 Ariez Azlan | All rights Reserved</p> </div> </div> </div> ));export default Footer;
+export const Footer = component$(() => ( <div class="mx-auto max-w-7xl px-6 py-12 md:flex md:items-center md:justify-between lg:px-8"> <div class="flex justify-center space-x-6 md:order-2"> <div class="mt-8 md:order-1 md:mt-0"> <p class="truncate text-sm text-gray-300">2024 Ariez | All rights Reserved</p> </div> </div> </div> ));export default Footer;
+
 /*
 export const Footer = component$(() => (
   
